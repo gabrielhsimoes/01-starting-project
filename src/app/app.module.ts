e98702bcd49a10a062db0e31caa7a423aecababb
Gabriel Nascimento <gabrielsimoesy@gmail.com>
2024-12-04 17:18:18 -0400

    adicionando módulos personalizados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,21 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
-import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
 import { UserComponent } from "./user/user.component";
 import { HeaderComponent } from "./header/header.component";
-import { TasksComponent } from "./tasks/tasks.component";
-import { TaskComponent } from "./tasks/task/task.component";
-import { NewTaskComponent } from "./tasks/new-task/new-task.component";
 import { SharedModule } from "./shared/shared.module";
+import { TasksModule } from "./tasks/tasks.module";
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    UserComponent,
-    TasksComponent,
-    TaskComponent,
-    NewTaskComponent
+    UserComponent
 
   ], //Somente para componentes não autômomos(Standalone)
   bootstrap: [AppComponent],
-  imports: [BrowserModule, FormsModule, SharedModule] //Para componentes autônomos
+  imports: [BrowserModule, SharedModule, TasksModule] //Para componentes autônomos
 })
 
 export class AppModule{}
